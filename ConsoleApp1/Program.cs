cfbad3356764263f6e986b1101a5988af09c4499
Karelo2705 <karlo.blaskovic@gmail.com>
2021-11-09 09:00:09 +0100

    maka text iz koda

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Karlo je caca"); 
-            Console.WriteLine("Karlo je caca 2, electric bogaloo");
+            Console.WriteLine("Karlo je caca 2");
         }
     }
 }
